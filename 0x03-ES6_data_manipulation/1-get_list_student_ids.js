d3d21e79b3f957bdc6292d5fd73ea80074347d92
OzimiWhoke <ozimi11zz@gmail.com>
2023-08-01 20:32:11 +0000

    getListStudentIds function

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,10 @@
+function getListStudentIds(students) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+
+  const ids = students.map((student) => student.id);
+  return ids;
+}
+
+export default getListStudentIds;
